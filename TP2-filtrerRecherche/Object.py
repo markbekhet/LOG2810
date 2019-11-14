34aa42fa8123d79f5f73efe80ba58e5d1a92e594
Marjorie <marjo.pomerleau@gmail.com>
2019-11-14 10:13:10 -0500

    Classe Object

---
+++ TP2-filtrerRecherche/Object.py
@@ -0,0 +1,38 @@
+class Object:
+    #Constructeur et instanciation des attributs
+    def __init__(self, type, id, name):
+        self._type = type
+        self._id = id
+        self._name = name
+        self._mass = mass
+
+    def getType(self):
+        return self._type
+
+    def getId(self):
+        return self._id
+
+    def getName(self):
+        return self._name
+
+    def getMass(self):
+        if type == "A":
+            self._mass = 1
+        elif type == "B":
+            self._mass = 3
+        elif type == "C":
+            self._mass = 5
+        else:
+            raise Exception("Type invalide")
+
+        return self._mass
+
+    def setType(self, type):
+        self._type = type
+
+    def setId(self, id):
+        self._id = id
+
+    def setName(self, name):
+        self._name = name
+
